58a7342a6daef44f8cb57898a884df43a043e969
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-02-25 21:21:27 -0300

    finished exercise 6

--- exercises/4.2/browser.js
+++ exercises/4.2/browser.js
@@ -1,9 +1,4 @@
-//Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
-//Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
-//Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
-//A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
-//Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
-//Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
 //Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 //Utilizando for , descubra qual o menor valor contido no array e imprima-o;
 //Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado;
@@ -14,6 +9,8 @@ let sum = 0;
 let maior = 0;
 let numeroatual = 0;
 let maiorNumero = 0;
+let contadorDeNumeros = 0;
+let impar = 0;
 
 // 2o  for (let index = 0; index < numbers.length; index += 1) {
 //   sum = numbers[index] += sum
@@ -59,11 +56,20 @@ let maiorNumero = 0;
 //        maiorNumero = numeroatual 
 //     }
 //     }
+//} console.log(maiorNumero)
+
+
+//6o for (let index = 0; index < numbers.length; index += 1){
+//    contadorDeNumeros = numbers[index];
+//
+//    if (contadorDeNumeros % 2 == 0){
+//
+//    } else {
+//      impar += 1;
+//    }
+//
 //}
-
-console.log(maiorNumero)
-
-
-
+//
+//console.log(impar)
 
 
