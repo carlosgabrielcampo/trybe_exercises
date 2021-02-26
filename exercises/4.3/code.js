c6cbe9f1d8522a6b3e6e24dcf9ccc0a4e39d532a
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-02-26 18:35:57 -0300

    exercise 1 completed

---
+++ exercises/4.3/code.js
@@ -0,0 +1,15 @@
+let repeticoes;
+
+repeticoes = 4;
+
+let asterisco;
+
+for (let index = 1; index <= repeticoes; index += 1){
+
+    asterisco = "*";
+    
+    let asteriscomulti = asterisco.repeat(repeticoes)
+  
+    console.log(asteriscomulti);
+}
+
