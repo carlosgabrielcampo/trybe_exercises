a738f2333fc4025612203ed58bfa17bef80749c7
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-02-26 19:07:09 -0300

    exercise 2 and 3 completed

--- exercises/4.3/code.js
+++ exercises/4.3/code.js
@@ -1,15 +1,48 @@
 let repeticoes;
 
-repeticoes = 4;
-
+repeticoes = 10;
+//
 let asterisco;
+//
+//1o  for (let index = 1; index <= repeticoes; index += 1){
+//
+//    asterisco = "*";
+//    
+//    let asteriscomulti = asterisco.repeat(repeticoes)
+//  
+//}
+
+
+//2o  asterisco = "*";
+//
+//for (let index = 1; index <= repeticoes; index += 1){
+//
+//    let asteriscomulti = asterisco.repeat(index)
+//
+//console.log(asteriscomulti)
+//   
+//}
+
+//asterisco = "*";
+//vazio = " ";
+
+//for (let index = 1; index <= repeticoes; index += 1){
+
+//   let asteriscomulti = vazio.repeat(repeticoes-index+1) + asterisco.repeat(index)
+
+//console.log(asteriscomulti)
+   
+//}
+
+asterisco = "*";
+vazio = " ";
 
 for (let index = 1; index <= repeticoes; index += 1){
 
-    asterisco = "*";
-    
-    let asteriscomulti = asterisco.repeat(repeticoes)
-  
-    console.log(asteriscomulti);
+    let asteriscomulti = vazio.repeat(repeticoes-index+1) + asterisco.repeat(index)
+
+console.log(asteriscomulti)
+   
 }
+
 
